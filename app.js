0dac5699f4a5e2ccd524cfa58898a6c6346d4bd6
J T <jtasharski@gmail.com>
2015-11-03 17:32:54 -0800

    controllers exist

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var galleryThumbs = ["assets/fish.jpg", "assets/bull.jpg", "assets/bird.jpg","assets/mushroom.jpg","assets/pig.jpg"];
+var app = angular.module('alexApp.controllers', ['ngRoute', 'ngResource']);
+
+app.controller('NavCtrl', ['$scope', '$location', function ($scope, $location) {
+
+}]);
+
+
+app.controller('GalleryCtrl', ['$scope', '$location', function ($scope, $location) {
+
+}]);
+var galleries = [];
